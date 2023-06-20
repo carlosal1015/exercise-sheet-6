bbac3fce01b2ed383f7856ee51041cb432b655ae
Carlos Aznarán Laos <caznaranl@uni.pe>
2023-06-20 17:18:44 -0500

    Update

---
+++ p34.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""Programa p34.py"""
+
+import sympy as sp
+
+
+x_1 = sp.Symbol("x_1", real=True)
+x_2 = sp.Symbol("x_2", real=True)
+x_3 = sp.Symbol("x_2", real=True)
+
+f_1 = 10 * x_1 - 2 * x_2**2 + x_2 - 2 * x_3 - 5
+f_2 = 8 * x_2**2 + 4 * x_3**2 - 9
+f_3 = 8 * x_2 * x_3 + 4
