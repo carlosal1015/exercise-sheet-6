ea89dacefa702e959159187fb689c32e561763d1
Carlos Aznarán Laos <caznaranl@uni.pe>
2023-06-21 15:22:38 -0500

    Update

---
+++ p11.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""Programa p11.py"""
+
+import numpy as np
+from tabulate import tabulate
+
+A = np.array(object=[[0.6, 0.2], [0.4, 0.8]], dtype=float)
+a = np.array(object=[1, 0], dtype=float)
