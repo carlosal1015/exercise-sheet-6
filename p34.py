ccaeefdf42f821975e07af9cff90dc3298ad5012
Carlos Aznarán Laos <caznaranl@uni.pe>
2023-06-21 08:09:13 -0500

    Update

--- p34.py
+++ p34.py
@@ -4,11 +4,45 @@
 
 import sympy as sp
 
+sp.init_printing()
 
 x_1 = sp.Symbol("x_1", real=True)
 x_2 = sp.Symbol("x_2", real=True)
-x_3 = sp.Symbol("x_2", real=True)
+x_3 = sp.Symbol("x_3", real=True)
 
-f_1 = 10 * x_1 - 2 * x_2**2 + x_2 - 2 * x_3 - 5
+f_1 = 10 * x_1 - 2 * x_2**2 - 2 * x_3 - 5
 f_2 = 8 * x_2**2 + 4 * x_3**2 - 9
 f_3 = 8 * x_2 * x_3 + 4
+
+X = sp.Matrix([f_1, f_2, f_3])
+Y = sp.Matrix([x_1, x_2, x_3])
+
+D_X_Y = X.jacobian(Y)
+
+print(f"{D_X_Y}")
+print(f"{D_X_Y.inv()}")
+
+print(f"{sp.simplify(D_X_Y * D_X_Y.inv())}")
+# f_1_prime_x_1 = sp.diff(f_1, x_1)
+# f_1_prime_x_2 = sp.diff(f_1, x_2)
+# f_1_prime_x_3 = sp.diff(f_1, x_3)
+
+# f_2_prime_x_1 = sp.diff(f_2, x_1)
+# f_2_prime_x_2 = sp.diff(f_2, x_2)
+# f_2_prime_x_3 = sp.diff(f_2, x_3)
+
+# f_3_prime_x_1 = sp.diff(f_3, x_1)
+# f_3_prime_x_2 = sp.diff(f_3, x_2)
+# f_3_prime_x_3 = sp.diff(f_3, x_3)
+
+# print(f"{f_1_prime_x_1}")
+# print(f"{f_2_prime_x_1}")
+# print(f"{f_3_prime_x_1}")
+
+# print(f"{f_1_prime_x_2}")
+# print(f"{f_2_prime_x_2}")
+# print(f"{f_3_prime_x_2}")
+
+# print(f"{f_1_prime_x_3}")
+# print(f"{f_2_prime_x_3}")
+# print(f"{f_3_prime_x_3}")
